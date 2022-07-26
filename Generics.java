a906a1f962528b7c22cfe60edaf19815f0a4b020
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-26 13:46:01 +0530

    Added: UC1

--- Generics.java
+++ Generics.java
@@ -1,7 +1,24 @@
 package javagenerics;
 
 public class Generics {
+	public static Integer compareTo(Integer num1, Integer num2, Integer num3) {
+
+		if (num1 > num2 && num1 > num3) {
+			return num1;
+		} else if (num2 > num3) {
+			return num2;
+		} else {
+			return num3;
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Generics!");
+
+		Generics integer = new Generics();
+
+		System.out.println("max Number for 1st test case=" + integer.compareTo(5, 3, 2));
+		System.out.println("max Number for 2nd test case=" + integer.compareTo(2, 6, 3));
+		System.out.println("max Number for 3rd test case=" + integer.compareTo(1, 2, 3));
+
 	}
 }
