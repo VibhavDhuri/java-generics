5a6a2299878c45b14a24288cb067bdb2454be42a
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-26 13:48:11 +0530

    Modified: UC1

--- Generics.java
+++ Generics.java
@@ -16,9 +16,9 @@ public class Generics {
 
 		Generics integer = new Generics();
 
-		System.out.println("max Number for 1st test case=" + integer.compareTo(5, 3, 2));
-		System.out.println("max Number for 2nd test case=" + integer.compareTo(2, 6, 3));
-		System.out.println("max Number for 3rd test case=" + integer.compareTo(1, 2, 3));
+		System.out.println("1st test case max integer:" + integer.compareTo(5, 3, 2));
+		System.out.println("2nd test case max integer:" + integer.compareTo(2, 6, 3));
+		System.out.println("3rd test case max integer:" + integer.compareTo(1, 2, 3));
 
 	}
 }
