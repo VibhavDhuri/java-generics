0758de0b4eaa494a01e3cabc0a6639ae9f32ca4b
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-26 15:29:37 +0530

    Added: UC3

--- Generics.java
+++ Generics.java
@@ -1,22 +1,29 @@
 package javagenerics;
 
 public class Generics {
-	public static float compareTo(float num1, float num2, float num3) {
 
-		if (num1 > num2 && num1 > num3) {
-			return num1;
-		} else if (num2 > num3) {
-			return num2;
-		} else {
-			return num3;
-		}
+	public static String compareTO(String name1, String name2, String name3) {
+
+		if (name1.length() > name2.length() && name1.length() > name3.length()) {
+			return name1;
+		} else if (name1.length() < name2.length() && name2.length() > name3.length()) {
+			return name2;
+		} else if (name1.length() < name3.length() && name2.length() < name3.length()) {
+			return name3;
+		} else if (name1.length() > name2.length() && name1.length() == name3.length()) {
+			return ("These two strings are equally great: " + name1 + " & " + name3);
+		} else if (name1.length() > name3.length() && name1.length() == name2.length()) {
+			return ("These two strings are equally great: " + name1 + " & " + name2);
+		} else if (name2.length() > name1.length() && name2.length() == name3.length()) {
+			return ("These two strings are equally great: " + name2 + " & " + name3);
+		} else
+			return ("All strings are equal.");
 	}
 
 	public static void main(String[] args) {
-
-		Generics number = new Generics();
-		System.out.println("1st test case max float:" + number.compareTo(4.1f, 0.6f, 1.2f));
-		System.out.println("2nd test case max float:" + number.compareTo(5.7f, 6.0f, 1.7f));
-		System.out.println("3rd test case max float:" + number.compareTo(4.1f, 2.1f, 5.2f));
+		Generics value = new Generics();
+		System.out.println("max Number for 1st test case=" + value.compareTO("Apple", "banana", "peach"));
+		System.out.println("max Number for 2nd test case=" + value.compareTO("peach", "banana", "peach"));
+		System.out.println("max Number for 3rd test case=" + value.compareTO("banana", "apple", "orange")); // position.
 	}
 }
